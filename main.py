985f469ccf1b95f453874ef43b38f19ae7bca3f8
AmikoJul <162383089+AmikoJul@users.noreply.github.com>
2024-06-22 13:40:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import os
 keep_alive()
 
 twitch_miner = TwitchChannelPointsMiner(
-    username="dudilidu_",
+    username="amikojul",
     password="2VOLlKANO81801",           # If no password will be provided, the script will ask interactively
     claim_drops_startup=False,                  # If you want to auto claim all drops from Twitch inventory on the startup
     priority=[                                  # Custom priority in this case for example:
